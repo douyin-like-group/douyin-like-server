6ea260f4c9c8ee48d28202b90612fae963a811cb
conchin <1439208355@qq.com>
2023-01-20 20:22:11 +0900

    重新maven install 了各个jar包

--- douyin-like-mapper/src/main/java/com/rocky/mapper/UsersMapper.java
+++ douyin-like-mapper/src/main/java/com/rocky/mapper/UsersMapper.java
@@ -2,6 +2,13 @@ package com.rocky.mapper;
 
 import com.rocky.my.mapper.MyMapper;
 import com.rocky.pojo.Users;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsersMapper extends MyMapper<Users> {
+
+    @Select("select 1 from users")
+    int testSql();
 }
--- douyin-like-service/src/main/java/com/rocky/service/impl/UsersServiceImpl.java
+++ douyin-like-service/src/main/java/com/rocky/service/impl/UsersServiceImpl.java
@@ -1,5 +1,34 @@
 package com.rocky.service.impl;
 
-public class UsersServiceImpl {
+import com.rocky.mapper.UsersMapper;
+import com.rocky.pojo.Users;
+import com.rocky.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+
+    @Autowired
+    private UsersMapper usersMapper;
+
+    @Transactional
+    @Override
+    public Users createUser(String email) {
+
+        return null;
+    }
+
+    @Override
+    public Users findById(long userId) {
+        return null;
+    }
+
+    @Override
+    public Users findByEmail(String email) {
+        return null;
+    }
     //todo
+
 }
