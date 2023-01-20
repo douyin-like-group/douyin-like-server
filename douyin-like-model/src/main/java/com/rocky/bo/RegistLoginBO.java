5c82ef24fff5bad8c1f5d9b03a2d05aabd0933c6
conchin <1439208355@qq.com>
2023-01-20 18:51:14 +0900

    add 注册登录BO

---
+++ douyin-like-model/src/main/java/com/rocky/bo/RegistLoginBO.java
@@ -0,0 +1,23 @@
+package com.rocky.bo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistLoginBO {
+
+    /**
+     * 注册登录传递的邮件地址和密码BO
+     */
+    @NotBlank(message = "邮件不能为空")
+    private String email;
+    @NotBlank(message = "密码不能为空")
+    private String password;
+
+}
--- douyin-like-service/src/main/java/com/rocky/service/UsersService.java
+++ douyin-like-service/src/main/java/com/rocky/service/UsersService.java
@@ -1,5 +1,29 @@
 package com.rocky.service;
 
+import com.rocky.pojo.Users;
+
 public interface UsersService {
+
     //todo
-}
+
+    /**
+     * 创建用户并且返回用户信息
+     */
+    public Users createUser(String email);
+
+    /**
+     * 根据用户主键查询用户
+     * @param userId
+     * @return
+     */
+    public Users findById(long userId);
+
+    /**
+     * 判断邮件是否存在，返回用户信息
+     */
+    public Users findByEmail(String email);
+
+
+
+
+ }
