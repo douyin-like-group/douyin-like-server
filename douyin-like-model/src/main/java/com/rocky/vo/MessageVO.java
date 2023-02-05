ae5b7b500a4f21fd61696e6f8bcbb648dbfc79dd
ChenMolu <993916778@qq.com>
2023-02-05 20:49:13 +0800

    初始化MsgService接口以及MsgController

--- douyin-like-model/src/main/java/com/rocky/vo/MessageVO.java
+++ douyin-like-model/src/main/java/com/rocky/vo/MessageVO.java
@@ -1,6 +1,7 @@
 package com.rocky.vo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.rocky.pojo.Message;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,13 +11,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MessageVO {
-    private long id;
+//    private long id;
+//
+//    private String content;
+//
+//    @JsonProperty("create_time")
+//    private String createTime;
+//    //消息发送时间 yyyy-MM-dd HH:MM:ss
 
-    private String content;
 
-    @JsonProperty("create_time")
-    private String createTime;
-    //消息发送时间 yyyy-MM-dd HH:MM:ss
+    /**
+     * 状态码，0-成功，其他值-失败
+     */
+    @JsonProperty("status_code")
+    private String statusCode;
+    /**
+     * 返回状态描述
+     */
+    @JsonProperty("status_msg")
+    private String statusMsg;
 
-
+    /**
+     * 用户列表
+     */
+    @JsonProperty("message_list")
+    private Message[] messageList;
 }
--- douyin-like-service/src/main/java/com/rocky/service/impl/MessageServiceImpl.java
+++ douyin-like-service/src/main/java/com/rocky/service/impl/MessageServiceImpl.java
@@ -1,6 +1,21 @@
 package com.rocky.service.impl;
 
-public class MessageServiceImpl {
+import com.rocky.pojo.Message;
+import com.rocky.service.MessageService;
+
+import java.util.List;
+
+public class MessageServiceImpl implements MessageService {
     //todo
+
+    @Override
+    public void createMsg(String fromUserId, String toUserId) {
+
+    }
+
+    @Override
+    public List<Message> queryList(String toUserId, Integer page, Integer pageSize) {
+        return null;
+    }
 
 }
