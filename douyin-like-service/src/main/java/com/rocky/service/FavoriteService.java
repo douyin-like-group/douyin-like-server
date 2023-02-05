8cab0f85787d3b15b52c84c28d838a3100438107
Jiang <1289932333@qq.com>
2023-02-05 18:14:57 +0800

    FavoriteService和FollowService接口的方法完成

--- douyin-like-service/src/main/java/com/rocky/service/FavoriteService.java
+++ douyin-like-service/src/main/java/com/rocky/service/FavoriteService.java
@@ -1,6 +1,46 @@
 package com.rocky.service;
 
+import com.rocky.vo.ResultVO;
+
 public interface FavoriteService {
     //todo
-    //Jiang
+
+    /**
+     * 点赞操作
+     * @param uid
+     * @param vid
+     * @return
+     */
+    public ResultVO like(long uid, long vid);
+
+    /**
+     * 取消点赞操作
+     * @param uid
+     * @param vid
+     * @return
+     */
+    public ResultVO unlike(long uid, long vid);
+
+    /**
+     * 获取点赞视频的列表
+     * @param uid
+     * @return
+     */
+    public ResultVO getlikeList(long uid);
+
+    /**
+     * 获取视频点赞数
+     * @param vid
+     * @return
+     */
+    public long getVideoBeLIkedCount(long vid);
+
+    /**
+     * 判断用户是否点赞视频
+     * @param uid
+     * @param vid
+     * @return
+     */
+    public boolean doesUserLikeVideo(long uid, long vid);
+
 }
