3559c6599a2ba7c13e1e06f6390e16f14941b6c2
Netter <2501259757@qq.com>
2023-02-05 16:24:23 +0800

    test

--- douyin-like-api/src/main/java/com/rocky/Application.java
+++ douyin-like-api/src/main/java/com/rocky/Application.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @MapperScan(basePackages = "com.rocky.mapper")
 public class Application {
     public static void main(String[] args) {
+        int a = 1;
             SpringApplication.run(Application.class, args);
     }
 }
