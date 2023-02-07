5b61b809c71c9bc6ea02532be400cd258b7941e4
conchin <1439208355@qq.com>
2023-02-07 20:00:02 +0900

    revise resultVO

--- douyin-like-model/src/main/java/com/rocky/vo/ResultVO.java
+++ douyin-like-model/src/main/java/com/rocky/vo/ResultVO.java
@@ -1,9 +1,15 @@
 package com.rocky.vo;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.jws.Oneway;
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
@@ -18,5 +24,22 @@ public class ResultVO {
     // 响应消息
 
     // 响应数据，可以是Object，也可以是List或Map等
+    @JsonIgnore
     private Object data;
+
+    @JsonIgnore
+    private String objectName;
+
+    @JsonAnyGetter
+    public Map<String, Object> getDataMap(){
+        Map<String, Object> map = new HashMap<>();
+        if(objectName==null){
+            objectName="data";
+        }
+        if(data==null){
+            data="";
+        }
+        map.put(objectName,data);
+        return map;
+    }
 }
