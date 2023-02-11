601696a185864fa8c08dd801ee868ce6a978b9fe
conchin <1439208355@qq.com>
2023-02-11 22:37:54 +0900

    测试评论成功

--- douyin-like-api/src/main/java/com/rocky/controller/CommentController.java
+++ douyin-like-api/src/main/java/com/rocky/controller/CommentController.java
@@ -27,7 +27,7 @@ public class CommentController extends BaseInfoProperties {
     CommentService commentService;
 
     @PostMapping("/action")
-    public ResponseEntity<ResultVO> actionComment(
+    public ResultVO actionComment(
             @RequestParam(required = true, value = "token") String token,
             @RequestParam(required = true, value = "video_id") String videoId,
             @RequestParam(required = true, value = "action_type") String actionType,
@@ -36,32 +36,39 @@ public class CommentController extends BaseInfoProperties {
 
         // 检查用户状态
         String userId = redis.get(REDIS_USER_TOKEN + ":" + token);
-        long sourceUserId;
+        ResultVO resultVO = new ResultVO();
         if(userId==null){
-            sourceUserId = 0L;
-        }else{
-            sourceUserId = Long.valueOf(userId);
+            resultVO.setStatusMsg("没有权限评论");
+            resultVO.setStatusCode(1);
+            return resultVO;
         }
+        long  sourceUserId = Long.valueOf(userId);
+
 
         Byte action = Byte.valueOf(actionType);
         CommentBO commentBO = new CommentBO(sourceUserId, Long.valueOf(videoId),
-                commentId.equals("") ? -1 : Long.valueOf(commentId), action, commentText);
+                (commentId==null)||commentId.equals("") ? -1 : Long.valueOf(commentId), action, commentText);
 
         CommentVO commentVO = commentService.queryComment(sourceUserId, commentBO);
 
-        ResultVO resultVO = new ResultVO();
         resultVO.setStatusCode(0);
         resultVO.setStatusMsg(action == 1 ? "评论成功" : "删除评论成功");
         resultVO.setData(commentVO);
         resultVO.setObjectName("comment");
 
-        return ResponseEntity.ok(resultVO);
+        return resultVO;
     }
 
     @GetMapping("/list")
-    public List<CommentVO> getCommentList( @RequestParam(required = true, value = "video_id") String vid) {
+    public ResultVO getCommentList( @RequestParam(required = true, value = "video_id") String vid) {
         List<CommentVO> commentList = commentService.getCommentList(Long.valueOf(vid));
-        return commentList;
+        ResultVO resultVO = new ResultVO();
+        resultVO.setStatusCode(0);
+        resultVO.setStatusMsg("获取评论列表成功" );
+        resultVO.setData(commentList);
+        resultVO.setObjectName("comment_list");
+
+        return resultVO;
     }
 
 }
