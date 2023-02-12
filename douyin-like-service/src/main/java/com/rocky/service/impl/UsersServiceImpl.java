4fe3746c0a6df9b3d75bf697632113b56a5e7f09
conchin <1439208355@qq.com>
2023-02-12 21:13:22 +0900

    remove conflict

--- douyin-like-service/src/main/java/com/rocky/service/impl/UsersServiceImpl.java
+++ douyin-like-service/src/main/java/com/rocky/service/impl/UsersServiceImpl.java
@@ -105,13 +105,7 @@ public class UsersServiceImpl extends BaseInfoProperties implements UsersService
         }
         String uToken = UUID.randomUUID().toString();
         //token作为key存储用户ID
-<<<<<<< Updated upstream
-
         redis.set(REDIS_USER_TOKEN+":"+uToken,user.getId().toString(),3600);
-
-=======
-        redis.set(REDIS_USER_TOKEN+":"+uToken,user.getId().toString(),3600);
->>>>>>> Stashed changes
         registerLoginVO.setUserId(user.getId());
         registerLoginVO.setToken(uToken);
         registerLoginVO.setStatusCode(0);
